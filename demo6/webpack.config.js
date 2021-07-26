475579650169c9fe55ff20ea007be787902a1807
yuchangchang <1346693197@qq.com>
2021-07-26 14:17:29 +0800

    - plugin

---
+++ demo6/webpack.config.js
@@ -0,0 +1,32 @@
+// ./webpack.config.js
+// import { Configuration } from 'webpack'
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const RemoveCommentsPlugin = require('./remove-comments-plugin')
+
+/**
+ * @type {Configuration}
+ */
+const config = {
+  entry: './src/main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  mode: 'none',
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: '你好，webpack',
+      template: './index.html',
+    }),
+    new CopyWebpackPlugin({
+      patterns: ['public'],
+    }),
+    new RemoveCommentsPlugin(),
+  ],
+}
+
+module.exports = config
